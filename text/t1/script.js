7b8eaa4b0f42e68a8118f01835aa5d7e29b4dbd0
HazelDaniel <hazeldaniel856@gmail.com>
2023-10-04 20:43:58 +0100

    first animation implemented(animation name: text bg slide). added functions for quicker dev time

--- text/t1/script.js
+++ text/t1/script.js
@@ -4,14 +4,24 @@ document.addEventListener("DOMContentLoaded", function () {
 	const textBGSlides = document.querySelectorAll(".text-bg-slide");
 	for (const el of textBGSlides) {
 		el.addEventListener("mouseenter", function () {
-			console.log("adding class");
-			el.classList.add("text-bg-toggle");
+			const slideCurtains = el.querySelectorAll("span");
+			if (!slideCurtains) return;
+			slideCurtains.forEach((curtain, idx)=>{
+				if (idx % 2) {
+					curtain.classList.add("slide-down");
+				} else {
+					curtain.classList.add("slide-up");
+				}
+			});
 		});
+
 		el.addEventListener("mouseleave", function () {
-			console.log("removing class");
-			el.classList.remove("text-bg-toggle");
+			const slideCurtains = el.querySelectorAll("span");
+			if (!slideCurtains) return;
+			slideCurtains.forEach((curtain, idx)=>{
+				curtain.classList.remove("slide-down");
+				curtain.classList.remove("slide-up");
+			});
 		});
-
 	}
-
 });
